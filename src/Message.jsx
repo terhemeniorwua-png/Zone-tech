d2b2e17247ad7af4f2fcbbaf26fce00e6cf72ac6
PHILIP <terhemeniorwua@gmail.com>
2026-08-03 16:30:09 +0100

    initial commit

---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+function List() {
+  return (
+    <ul>
+      <li className="list">Age</li>
+      <li className="list">Age</li>
+      <li className="list">Age</li>
+      <li className="list">Age</li>
+      <li className="list">Age</li>
+      <li className="list">Age</li>
+    </ul>
+  );
+}
+
+export default List;
