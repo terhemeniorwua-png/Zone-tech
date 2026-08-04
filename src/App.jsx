45cd7d28ab8b9e88393e0776fc7370f4f3034fa2
PHILIP <terhemeniorwua@gmail.com>
2026-08-04 16:15:22 +0100

    add the culture section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import OurService from './components/OurService'
+import Culture from './components/Culture'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
    <Navbar />
    <Hero />
    <OurService />
+   <Culture />
     </>
   )
 }
---
+++ src/components/Culture.jsx
@@ -0,0 +1,24 @@
+
+import culturesimg from '../assets/images/cultures.png'
+
+
+function Culture(){
+
+    return(
+
+        <>
+            <div className='items-center grid-cols-2 pt-20 md:grid'>
+                <div>
+                    <img src={ culturesimg } alt="img" />
+                </div>
+                <div>
+                    <h4 className='pb-5 text-xl font-bold textplaceholder-blue-900'>Culture of <span className='text-orange-400'> Our company</span></h4>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam laudantium ex natus voluptatem officiis accusantium distinctio architecto in nesciunt. Harum, nam perferendis deserunt aspernatur ex porro tempore commodi id quos.</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default  Culture
