f79b6cbefc62be059f962c629f216e73047c9126
PHILIP <terhemeniorwua@gmail.com>
2026-08-04 23:51:15 +0100

    add the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,16 +7,16 @@ function Footer(){
         <>
         
         <footer>
-            <div>
+            <div className='grid-cols-2 pt-20 text-gray-400 max-md:w-[70%] max-md:pl-5 pb-20 md:grid'>
                 <div>
-                    <img src={ logo } alt="logo" />
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat debitis culpa provident dignissimos, voluptas est at dolorum dolores dolorem distinctio hic neque voluptatibus atque. Minus odit facilis ducimus enim voluptatem.</p>
+                    <img src={ logo } alt="logo" className='w-[30%]'/>
+                    <p className='text-sm text-gray-400'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat debitis culpa provident dignissimos, voluptas est at dolorum dolores dolorem distinctio hic neque voluptatibus atque. Minus odit facilis ducimus enim voluptatem.</p>
                 </div>
 
-                <div>
-                    <div>
-                        <h3 className='text-orange-400 underline'>About Us</h3>
-                        <ul>
+                <div className='grid-cols-3 md:grid max-md:text-left max-md:pl-2'>
+                    <div className='space-y-2'>
+                        <h3 className='font-semibold text-orange-400 underline '>About Us</h3>
+                        <ul className='space-y-5 text-sm'>
                             <li>Home</li>
                             <li>About Us</li>
                             <li>How we work</li>
@@ -25,24 +25,24 @@ function Footer(){
                             <li>Blog</li>
                         </ul>
                     </div>
-                    <div>
-                         <h3 className='text-orange-400 underline'>Location</h3>
-                       <p>230, 3991 ipsum, dolor sit amet consectetur adipisicing elit. Suscipit amet nesciunt eligendi </p>
+                    <div className='space-y-5'>
+                         <h3 className='font-semibold text-orange-400 underline '>Location</h3>
+                       <p className='text-sm text-gray-400'>230, 3991 ipsum, dolor sit amet consectetur adipisicing elit. Suscipit amet nesciunt eligendi </p>
 
-                       <p>230, 3991, ipsum dolor sit amet consectetur adipisicing elit exercitationem tempore.</p>
+                       <p className='text-sm text-gray-400'>230, 3991, ipsum dolor sit amet consectetur adipisicing elit exercitationem tempore.</p>
                     </div>
-                      <div>
-                         <h3 className='text-orange-400 underline'>Location</h3>
-                       <ul>
-                        <li>Contact me on: <a href="https://wa.me/09166354571" className='text-blue-700 hover:underline'>Whatsapp</a></li>
-                        <li><a href="mailto:terhemeniorwua@gmail.com" className='text-blue-700 hover:underline'>terhemeniorwua@gmail.com</a></li>
+                      <div className='space-y-5'>
+                         <h3 className='font-semibold text-orange-400 underline '>Contact Us</h3>
+                       <ul className='space-y-5 text-sm'>
+                        <li>Contact me on: <a href="https://wa.me/09166354571" className='underline hover:text-blue-700'>Whatsapp</a></li>
+                        <li>Mail me via: <a href="mailto:terhemeniorwua@gmail.com" className='underline hover:text-blue-700'>terhemeniorwua@gmail.com</a></li>
                        </ul>
                     </div>
                 </div>
             </div>
         </footer>
         
-        
+        <div className='h-5 bg-gray-800 w-[50%] m-auto'></div>
         </>
     )
 }
