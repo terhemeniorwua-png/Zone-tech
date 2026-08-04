5d72b14b88d2b4ed36b1e9ce0d5374c447509659
PHILIP <terhemeniorwua@gmail.com>
2026-08-04 15:17:56 +0100

    implement the responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,26 +1,53 @@
 
 import heroimg from '../assets/images/heroimg.png'
+import dell from '../assets/images/dell.png'
+import eye from '../assets/images/eye.png'
+import circle from '../assets/images/circle.png'
+import northface from '../assets/images/nothface.png'
+import nasa from '../assets/images/nasa.png'
+import huawi from '../assets/images/huawi.png'
+
 
 function Hero(){
 
     return (
         <>
-            <div className="grid grid-cols-2">
+            <div className="grid-cols-2 md:grid">
 
                <div>
-                <h1 className='font-bold text-left text-blue-900 w-[80%] tracking-wider'>Create your Success
-                       <p className='text-6xl text-orange-400'>Business</p>
+                <h1 className='font-bold md:text-left text-blue-900 md:w-[80%] tracking-wider'>Create your Success
+                       <p className='text-5xl text-center text-orange-400'>Business</p>
                      </h1>
-                     <p className='w-[80%]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla voluptatibus est beatae, distinctio culpa ullam nostrum aliquid eum nesciunt laboriosam.</p>
-                     <div className='px-5 py-2 text-white text-[15px] bg-orange-400 rounded-3xl w-[20%] mt-2'>
+                     <p className='md:w-[80%] max-md:mb-10 max-md:text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla voluptatibus est beatae, distinctio culpa ullam nostrum aliquid eum nesciunt laboriosam.</p>
+                     <div className='max-md:m-auto px-5 py-2 text-white text-[15px] bg-orange-400 rounded-3xl md:w-[20%] md:mt-2'>
                         Explore
                      </div>
                 </div> 
                 
                 <div className=''>
-                    <div className='bg-black rounded-full h-72 w-72 opacity-20'></div>
-                    <img src={ heroimg } alt="worker" className='w-[33%] absolute top-24'/>
+                    <div className='m-auto bg-black rounded-full md:ml-16 max-md:mt-5 h-72 w-72 opacity-20'></div>
+                    <img src={ heroimg } alt="worker" className='w-[90%] -bottom-56 md:w-[33%] absolute  md:top-24'/>
                 </div>
+            </div>
+            
+            <div>
+                <h5 className='pt-12 font-bold max-md:text-sm max-md:pt-20'>
+                    <i class="fa-solid fa-chevron-left"></i>
+                    
+                    <span>Trusted by <span className='text-orange-400'>100+ Companies</span> Worldwide </span>
+                    <i class="fa-solid fa-chevron-right"></i>
+                </h5>
+                
+                <div className='grid justify-center grid-cols-3 gap-10 pt-10 gap md:flex '>
+                    <img src={ dell } alt="dell" />
+                    <img src={ eye } alt="eye" />
+                    <img src={ circle } alt="circl" />
+                    <img src={ northface } alt="northface" />
+                    <img src={ nasa } alt="nasa" />
+                    <img src={ huawi } alt="huawi" />
+                </div>
+
+
             </div>
         
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,43 @@ import logo from '../assets/images/logo.png'
 
 function Navbar(){
 
+ const handleMobileNav = (e) =>{
+    let clicked = e.target; 
+    if(clicked){
+        clicked.classList.toggle('')
+    }
+    clicked.classList.includes('hidden') && 'X'
+ }
+
+
+
+    
+
     return (
         <header>
-            <div className='text-[#000] font-semibold flex justify-between items-center'>
+
+        {/* Mobile Nav */}
+        <div className='flex items-center justify-between md:hidden'>
+            <div>
+                   <img src={ logo } alt="Logo" className='p-0 w-44'/>
+                </div>
+            <div><i class="fa-solid fa-bars text-2xl" onClick={ (e)=>handleMobileNav(e) }></i></div>
+        </div>
+        
+        <div className='bg-[#000000] py-5 leading-8 rounded-lg hidden'>
+            <ul className='font-semibold text-center text-white'>
+                        <li>Home</li>
+                        <li>Services</li>
+                        <li>How We Work</li>
+                        <li>Our Team</li>
+                        <li>Careers</li>
+                        <li>Blog</li>
+                    </ul>
+        </div>
+
+
+            {/* Desktop Nav */}
+            <div className='text-[#000] font-medium flex justify-between items-center max-md:hidden'>
                 <div>
                    <img src={ logo } alt="Logo" className='p-0 w-44'/>
                 </div>
